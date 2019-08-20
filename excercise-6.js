52c3c22ed654cac1d5a34124e722be8d1c8dcc8f
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-21 00:49:29 +0700

    add ex 6

--- excercise-6.js
+++ excercise-6.js
@@ -1,5 +1,51 @@
 function meleeRangedGrouping (str) {
-  //your code here
+ 
+  var groups = [];
+//   var indeks = 0;
+  var temp = '';
+
+  for (var i = 0; i < str.length; i++) {
+      if(str[i] === ',') {
+        groups.push(temp);
+        // groups.push([]);
+        // indeks++;
+        temp = '';
+      } 
+     else if (i === str.length-1) {
+         temp += str[i];
+        groups.push(temp);
+    }
+      else {
+          temp += str[i];       
+      }
+      
+  }
+  console.log(groups);
+  var groupSplit = [[]];
+//   var indeks = 0;
+  var temp2 = '';
+  for (var i = 0; i < temp.length; i++) {
+      for (var j = 0; j < temp[i].length; j++) {
+          if (temp[i][j] === '-') {
+            groupSplit[i].push(temp2);
+            // groupSplit.push([]);
+            // indeks++;
+            temp2 = '';
+          } else if (j === temp[i].length-1){
+              temp2 += temp[i][j];
+              groupSplit[i].push(temp2);
+              temp2 = '';
+          }
+          else {
+              temp2 += temp[i][j];
+          }
+      }
+      if (i === temp.length-1) {
+        break;
+      }
+      groupSplit.push([]);
+  }
+  console.log(groupSplit);
 }
 
 // TEST CASE
@@ -7,7 +53,7 @@ function meleeRangedGrouping (str) {
 console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
 // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
 
-console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
 // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
 
-console.log(meleeRangedGrouping('')); // []
+// console.log(meleeRangedGrouping('')); // []
