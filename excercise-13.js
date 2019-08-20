e3d42657e38c3e773b42caa809ae86696cc4b6c0
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-20 14:10:08 +0700

    fixed excercise-13

--- excercise-13.js
+++ excercise-13.js
@@ -1,16 +1,34 @@
 function sorting(arrNumber) {
-    // code di sini
+    for (var i = 0; i < arrNumber.length; i++) {
+        for (var j = 0; j < arrNumber.length; j++) {
+            if (arrNumber[j] > arrNumber[j + 1]) {
+                var temp = arrNumber[j];
+                arrNumber[j] = arrNumber[j + 1];
+                arrNumber[j + 1] = temp;
+            }
+        }
+    }
+    return arrNumber;
 }
-
-function getTotal(arrNumber) {
-  // code di sini
+function getTotal(listSort) {
+    sorting(listSort);
+    var count = 0;
+    if (listSort.length === 0) {
+        return '\'\'';
+    }
+    for (var i = 0; i < listSort.length; i++) {
+        if (listSort[i] === listSort[listSort.length - 1]) {
+            count++;
+        }
+    }
+    return 'angka paling besar adalah ' + listSort[listSort.length - 1] + ' dan jumlah kemunculan sebanyak ' + count + ' kali';
 }
-
 function mostFrequentLargestNumbers(arrNumber) {
-  var listSort = sorting(arrNumber);
-  var countHighest = getTotal(listSort);
-  return countHighest;
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
 }
+
 
 console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
 //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
