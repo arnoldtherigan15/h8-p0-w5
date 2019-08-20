3b22a8332bb2c10b8656dc91e082f4690451be78
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-20 20:21:44 +0700

    fixed excercise - 14

--- excercise-14.js
+++ excercise-14.js
@@ -1,21 +1,55 @@
 function changeVocals (str) {
-    //code di sini
+    var vokal = ['a','i','u','e','o','A','I','U','E','O'];
+    var konsonan = ['b','j','v','f','p','B','J','V','F','P'];
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < vokal.length; j++) {
+            if (str[i] === vokal[j]) {
+                str[i] = konsonan[j];
+            } 
+        }
+    }
+    return str;
 }
 
 function reverseWord (str) {
-  //code di sini
+    var reverse = [];
+    for (var i = str.length-1; i >= 0; i--) {
+        reverse.push(str[i]);
+    }
+    return reverse;
 }
 
 function setLowerUpperCase (str) {
-  //code di sini
+  var result = [];
+  for (var i = 0; i < str.length; i++) {
+      if (str[i] === str[i].toLowerCase()) {
+          result.push(str[i].toUpperCase());
+      } else {
+        result.push(str[i].toLowerCase());
+      }
+  }
+  return result;
 }
 
 function removeSpaces (str) {
-  //code di sini
+  for (var i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+          str[i] = '';
+      }
+  }
+  return str;
 }
 
 function passwordGenerator (name) {
-  //code di sini
+    name = name.split('');
+    if (name.length < 5) {
+      return 'Minimal karakter yang diinputkan adalah 5 karakter';
+  }
+  var name = changeVocals(name);
+  var name = reverseWord(name);
+  var name = setLowerUpperCase(name);
+  var name = removeSpaces(name);
+  return name.join('');
 }
 
 console.log(passwordGenerator('Sergei Dragunov')); // 'VPNVGBRdJFGRFs'
